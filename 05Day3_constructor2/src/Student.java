01a1c3a13f327f8e0a52bcaa7f57b6a0e71ed68a
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-25 23:01:34 +0900

    05Day3_constructor2

---
+++ 05Day3_constructor2/src/Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+  //1. 멤버변수 영역
+	String name; //이름
+	int age;//나이 
+	String address;//주소
+	
+	//2. 생성자(constructor)영역
+	//2-1 명시적 생성자를 작성
+	//- 접근제한자 클래스이름(인자){}
+	//public Student() {// 인자(매개변수)없는 생성자: 기본생성자(default 생성자)
+	//	System.out.println("이게바로 생성자 만드는 공식이야");
+//	}
+
+	 // 묵시적 기본생성자
+	// 개발자가 생성자를 명시적으로 작성하지 않으면 기본생성자코드를 jdk가 자동으로 삽입, 객체 생성에 사용함
+}
